7ba9a527919a0d410a4de5859fbb3a916bdaf323
jtay <jostinezequiel09@gmail.com>
2025-05-24 21:06:44 -0400

    resolve issues

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 class Config():
     SECRET_KEY = 'Hola'
 
@@ -5,7 +7,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '1v19514#'
+    MYSQL_PASSWORD = getenv('PASSWORD_DB')
     MYSQL_DB = 'Estacion_policial'    
 
 config = {
